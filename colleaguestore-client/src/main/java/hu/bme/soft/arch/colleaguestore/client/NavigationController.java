ec043dad115000f7adb2d6b32843d4394577d02e
Dévényi Lóránt <d.lorant92@gmail.com>
2017-11-11 14:26:05 +0100

    Defautl JSP Pages with ajax

---
+++ colleaguestore-client/src/main/java/hu/bme/soft/arch/colleaguestore/client/NavigationController.java
@@ -0,0 +1,16 @@
+package hu.bme.soft.arch.colleaguestore.client;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "navigationController", eager = true)
+@RequestScoped
+public class NavigationController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public String moveToPage1() {
+		return "page1";
+	}
+}
