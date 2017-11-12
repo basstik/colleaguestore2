89bae172c66043ba1692ddf7ba7ffed9cccf6988
Dévényi Lóránt <d.lorant92@gmail.com>
2017-11-12 18:20:42 +0100

    Response 200

--- colleaguestore-facade/src/main/java/hu/bme/soft/arch/colleaguestore/facade/PersonFacadeBean.java
+++ colleaguestore-facade/src/main/java/hu/bme/soft/arch/colleaguestore/facade/PersonFacadeBean.java
@@ -1,11 +1,23 @@
 package hu.bme.soft.arch.colleaguestore.facade;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 
 import hu.bme.soft.arch.colleaguestore.domain.dto.PagingPersonDTO;
@@ -59,9 +71,43 @@ public class PersonFacadeBean implements PersonFacade {
 		personPM.remove(id);
 	}
 
+	// curl -u admin:admin
+	// http://152.66.245.130:84/restconf/operational/opendaylight-inventory:nodes/node/openflow:84590320485820/group/150010/group-statistics/
+
 	@Override
 	public void print() {
-		logger.info("------------------////////--------__!!!!!!!!!!!!!!!!!!!!!");
+		try {
+			String url = "http://152.66.245.130:84/restconf/operational/opendaylight-inventory:nodes/node/openflow:84590320485820/group/150010/group-statistics/";
 
+			CredentialsProvider credsProvider = new BasicCredentialsProvider();
+			// Credentials defaultcreds = new UsernamePasswordCredentials("admin", "admin");
+			credsProvider.setCredentials(new AuthScope("152.66.245.130", AuthScope.ANY_PORT),
+					new UsernamePasswordCredentials("admin", "admin"));
+
+			CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+			HttpGet request = new HttpGet(url);
+
+			// add request header
+			request.addHeader("User-Agent", "curl/7.35.0");
+			CloseableHttpResponse response = client.execute(request);
+			// HttpResponse response;
+			// response = client.execute(request);
+
+			System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
