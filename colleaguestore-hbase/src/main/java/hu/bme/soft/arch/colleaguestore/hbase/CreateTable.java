e30c8e8aea281e61a222e6e7cfce67e898a71a62
Dévényi Lóránt <d.lorant92@gmail.com>
2017-11-12 13:19:17 +0100

    Hbase moduel, one warning

--- colleaguestore-hbase/src/main/java/hu/bme/soft/arch/colleaguestore/hbase/CreateTable.java
+++ colleaguestore-hbase/src/main/java/hu/bme/soft/arch/colleaguestore/hbase/CreateTable.java
@@ -2,30 +2,31 @@ package hu.bme.soft.arch.colleaguestore.hbase;
 
 import java.io.IOException;
 
-//import org.apache.hadoop.hbase.HBaseConfiguration;
-//import org.apache.hadoop.hbase.HColumnDescriptor;
-//import org.apache.hadoop.hbase.HTableDescriptor;
-//import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 public class CreateTable {
 
 	public void createTable() throws IOException {
-		//
-		// // Instantiating configuration class
-		// Configuration con = HBaseConfiguration.create();
-		//
-		// // Instantiating HbaseAdmin class
-		// HBaseAdmin admin = new HBaseAdmin(con);
-		//
-		// // Instantiating table descriptor class
-		// HTableDescriptor tableDescriptor = new HTableDescriptor("emp3");
-		//
-		// // Adding column families to table descriptor
-		// tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-		// tableDescriptor.addFamily(new HColumnDescriptor("professional"));
-		//
-		// // Execute the table through admin
-		// admin.createTable(tableDescriptor);
-		// System.out.println(" Table created ");
+
+		// Instantiating configuration class
+		Configuration con = HBaseConfiguration.create();
+
+		// Instantiating HbaseAdmin class
+		HBaseAdmin admin = new HBaseAdmin(con);
+
+		// Instantiating table descriptor class
+		HTableDescriptor tableDescriptor = new HTableDescriptor("emp3");
+
+		// Adding column families to table descriptor
+		tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+		tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+
+		// Execute the table through admin
+		admin.createTable(tableDescriptor);
+		System.out.println(" Table created ");
 	}
 }
