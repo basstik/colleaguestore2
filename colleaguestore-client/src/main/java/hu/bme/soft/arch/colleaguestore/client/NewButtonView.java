2f28a9e4c23ae0b6203bfb203be505d75fe3ad12
Dévényi Lóránt <d.lorant92@gmail.com>
2017-11-28 10:37:19 +0100

    NewView

---
+++ colleaguestore-client/src/main/java/hu/bme/soft/arch/colleaguestore/client/NewButtonView.java
@@ -0,0 +1,19 @@
+package hu.bme.soft.arch.colleaguestore.client;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class NewButtonView {
+
+	public void buttonAction(ActionEvent actionEvent) {
+		addMessage("Welcome to Primefaces!!");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+}
