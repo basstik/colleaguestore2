ca5d95067cb6d153a179ce516450985c8f5f8f19
Lorant <d.lorant92@gmail.com>
2017-12-08 12:41:37 +0100

    Remove unused test

--- colleaguestore-persistence/src/main/java/hu/bme/soft/arch/colleaguestore/persistence/entity/Roles.java
+++ colleaguestore-persistence/src/main/java/hu/bme/soft/arch/colleaguestore/persistence/entity/Roles.java
@@ -9,15 +9,15 @@ import javax.persistence.Table;
 @Table(name = "roles")
 public class Roles {
 
-	// INSERT INTO users(username, password) VALUES ('admin', 'admin');
-	// INSERT INTO users(username, password) VALUES ('view', 'view');
-	// INSERT INTO roles(rolename) VALUES ('LOGIN');
-	// INSERT INTO roles(rolename) VALUES ('ADMIN');
-	// INSERT INTO roles(rolename) VALUES ('VIEW');
-	// INSERT INTO user_roles(username, role) VALUES ('admin', 'LOGIN');
-	// INSERT INTO user_roles(username, role) VALUES ('admin', 'ADMIN');
-	// INSERT INTO user_roles(username, role) VALUES ('view', 'LOGIN');
-	// INSERT INTO user_roles(username, role) VALUES ('view', 'VIEW');
+//	 INSERT INTO users(username, password) VALUES ('admin', 'admin');
+//	 INSERT INTO users(username, password) VALUES ('view', 'view');
+//	 INSERT INTO roles(rolename) VALUES ('LOGIN');
+//	 INSERT INTO roles(rolename) VALUES ('ADMIN');
+//	 INSERT INTO roles(rolename) VALUES ('VIEW');
+//	 INSERT INTO user_roles(username, role) VALUES ('admin', 'LOGIN');
+//	 INSERT INTO user_roles(username, role) VALUES ('admin', 'ADMIN');
+//	 INSERT INTO user_roles(username, role) VALUES ('view', 'LOGIN');
+//	 INSERT INTO user_roles(username, role) VALUES ('view', 'VIEW');
 	@Id
 	@Column(name = "rolename", length = 50, nullable = false)
 	private String roleName;
